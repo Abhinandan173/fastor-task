3a2b3cec2dda28bc538aa56d82c8fce5afd4c6dd
Abhinandan173 <abhibaloji7@gmail.com>
2022-02-03 17:03:17 +0530

    -

--- src/Screens/clickedRestorent.js
+++ src/Screens/clickedRestorent.js
@@ -30,7 +30,7 @@ const ClickedRestorent = () => {
 
             <div style={{display:'flex', flexDirection:'row', justifyContent:'center',}} className='share-btn'  >
             <ShareSocial
-                    style={{
+                style={{
                     width:'50%',
                     background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
                     borderRadius: 3,
